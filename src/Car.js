0d32eaf6e57a8a24fa016a318d5a4bb6ffa2f274
Marius Grondahl <marius@grondahldesign.no>
2019-05-13 10:51:03 +0200

    Feature - List cars

--- src/Car.js
+++ src/Car.js
@@ -15,12 +15,20 @@ const carHeader = {
 const carDescription = {
 };
 
-let i = 0;
-function findIndex () {
-};
 
+
+const CarsArray = require('./BilData');
+console.log(CarsArray)
 
 class Car extends React.Component {
+
+  createCarList () {
+
+    CarsArray.map((car) =>
+    <li>{car.Merke}</li>
+  );
+  }
+
   render() {
     return (
       <div style={carWrapper}>
@@ -28,6 +36,7 @@ class Car extends React.Component {
         <Image backgroundImage={'../img/zoey.jpg'}/>
         <Headline>Tittel henta fra json her</Headline>
         </div>
+        <div>{this.createCarList}</div>
         <p className={carDescription}>
           Description henta fra json her
         </p>
